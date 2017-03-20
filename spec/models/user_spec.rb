01be5374e2629dd3363206614ce3e7c14bb374e7
cwkosman <cwkosman@gmail.com>
2017-03-20 21:52:03 +0000

    Specs: valid credentials is valid, passwords mismatch is invalid

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    let(:user) do
+      User.create(
+        first_name: 'Han',
+        last_name: 'Solo',
+        email: 'hansolo@mfalcon.net',
+        password: 'easychewie',
+        password_confirmation: 'easychewie'
+      )
+    end
+    it 'should be valid when created with valid credentials' do
+      expect(user).to be_valid
+    end
+    it 'should not be valid when passwords don\'t match' do
+      user.password_confirmation = 'easych3wie'
+      user.valid?
+      expect(user.errors.full_messages).to eq(['Password confirmation doesn\'t match Password'])
+    end
+  end
+end
