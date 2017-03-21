226368a9c49805b31c7006f6e78dfb549e9f4fb4
cwkosman <cwkosman@gmail.com>
2017-03-21 21:00:37 +0000

    Check for all faked data instead of one instance

--- spec/features/home_pages_spec.rb
+++ spec/features/home_pages_spec.rb
@@ -19,7 +19,7 @@ RSpec.feature 'Visitor navigates to home page', type: :feature, js: true do
     # ACT
     visit root_path
     # DEBUG / VERIFY
-    save_screenshot
-    expect(page).to have_css 'article.product'
+     # save_screenshot
+    expect(page).to have_css 'article.product', count:10
   end
 end
